5e86e84ee3463b11e7f8e2d92bbcf406700dd31a
Santtu Pajukanta <santtu@pajukanta.fi>
2013-03-28 14:47:50 +0200

    ssh

--- install.bash
+++ install.bash
@@ -3,3 +3,7 @@
 grep -q 'dotfiles/bash' $HOME/.bashrc || (echo 'source $HOME/Work/dotfiles/bash/*.bash' >> $HOME/.bashrc)
 ln -sf $HOME/Work/dotfiles/git/gitconfig $HOME/.gitconfig
 ln -sf $HOME/Work/dotfiles/vim/vimrc $HOME/.vimrc
+
+mkdir -p $HOME/.ssh
+chmod 700 $HOME/.ssh
+ln -sf $HOME/Work/dotfiles/ssh/config $HOME/.ssh
