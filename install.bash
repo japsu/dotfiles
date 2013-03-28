64800eb7dce8d4ce90694b4c89c23793b9f7dd60
Santtu Pajukanta <santtu@pajukanta.fi>
2013-03-28 14:43:39 +0200

    add vim

--- install.bash
+++ install.bash
@@ -2,3 +2,4 @@
 
 grep -q 'dotfiles/bash' $HOME/.bashrc || (echo 'source $HOME/Work/dotfiles/bash/*.bash' >> $HOME/.bashrc)
 ln -sf $HOME/Work/dotfiles/git/gitconfig $HOME/.gitconfig
+ln -sf $HOME/Work/dotfiles/vim/vimrc $HOME/.vimrc
