d21bcff166d84d50177221dbbc874d21bbd1edfa
Santtu Pajukanta <santtu@pajukanta.fi>
2013-04-08 10:25:11 +0300

    nvm

--- install.bash
+++ install.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 grep -q 'dotfiles/bash' $HOME/.bashrc || (echo 'source $HOME/Work/dotfiles/bash/*.bash' >> $HOME/.bashrc)
+grep -q 'nvm' $HOME/.bashrc || (echo 'source $HOME/System/nvm/nvm.sh' >> $HOME/.bashrc)
+
 ln -sf $HOME/Work/dotfiles/git/gitconfig $HOME/.gitconfig
 ln -sf $HOME/Work/dotfiles/vim/vimrc $HOME/.vimrc
 
